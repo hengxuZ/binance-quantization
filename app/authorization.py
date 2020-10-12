f4ccf43c8dde483b39767931c844fee4600be376
幸福村的二娃 <xingfucundeerwa@xingfucundeerwadeMacBook-Pro.local>
2020-10-12 17:45:26 +0800

    调整内容

--- app/authorization.py
+++ app/authorization.py
@@ -1,8 +1,8 @@
 # Get an Key and Secret
 # https://www.binance.com/restapipub.html
 
-api_key = '3onatbT2duhKqba4LtTeWFFuFs4b8meKFjUqHZS3Z5Yq0NZGlFnG9pzVBCHakdIg'
-api_secret = 'Q0bXNbYwMofala98TY1eMEWlA8lKi4prWJOksDpLtBvdWOHu0mRSXavdwRkE3y0E'
+api_key = ''
+api_secret = ''
 recv_window = 5000
 
-dingding_token = "618c195120b60e6832b6e21e7c10043605dfb29955efaad8667da55daaf5f75a"
+dingding_token = ""
