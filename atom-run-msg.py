d4608fc25f4ef1cd71e7cde3817114b4253808eb
hengxuZ <m15583730895@163.com>
2020-10-28 11:22:51 +0800

    调整预警

--- atom-run-msg.py
+++ atom-run-msg.py
@@ -13,24 +13,28 @@ msg = Message()
 def loop_fun():
     while True:
         if runbet.get_buy_price() >= binan.get_ticker_price(runbet.get_cointype()):
-            order_id = msg.buy_limit_msg(runbet.get_cointype(),runbet.get_quantity(),runbet.get_buy_price())
-            if order_id['orderId']:
-                runbet.modify_price(runbet.get_buy_price(),runbet.get_step()+1)
-                time.sleep(60) # 挂单后，停止运行1分钟
-
+            try:
+                res = msg.buy_limit_msg(runbet.get_cointype(),runbet.get_quantity(),runbet.get_buy_price())
+                if res['orderId']:
+                    runbet.modify_price(runbet.get_buy_price(),runbet.get_step()+1)
+                    time.sleep(60) # 挂单后，停止运行1分钟
+            except:
+                msg.dingding_warn('没有买入成功响应结果:'+str(res))
         elif runbet.get_sell_price() < binan.get_ticker_price(runbet.get_cointype()):
-
-            order_id = msg.sell_limit_msg(runbet.get_cointype(),runbet.get_quantity(), runbet.get_sell_price())
-            if order_id['orderId']:
-                runbet.modify_price(runbet.get_sell_price(),runbet.get_step()-1)
-                time.sleep(60)
+            try:
+                res = msg.sell_limit_msg(runbet.get_cointype(),runbet.get_quantity(), runbet.get_sell_price())
+                if res['orderId']:
+                    runbet.modify_price(runbet.get_sell_price(),runbet.get_step()-1)
+                    time.sleep(60)
+            except:
+                msg.dingding_warn('没有买入成功响应结果:'+str(res))
         else:
             print("当前市价：{market_price}。未能满足交易,继续运行".format(market_price = binan.get_ticker_price(runbet.get_cointype())))
 
 
 if __name__ == "__main__":
-    try:
-        loop_fun()
-    except Exception as e:
-        error_info = "报警：" + str(e)
-        msg.dingding_warn(error_info)
+    #try:
+    loop_fun()
+    #except Exception as e:
+        #error_info = "报警：" + str(e)
+        #msg.dingding_warn(error_info)
